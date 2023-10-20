6762e0cd62a1ba5cd66b6974a07f4c4d7b68361f
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-10-20 18:01:04 +0300

    add

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const now = new Date();
+// const now = new Date();
 
-console.log(`Today ${now.getFullYear()} year`);
+// console.log(`Today ${now.getFullYear()} year`);
+
+// console.log("nodevon v roboti...")
+
+const users = require("./users")
+
+console.log(users)
+
+const { animals } = require("./users");
+
+console.log(animals)
+
+// const { getCurrentMonth } = require("./dateFunctions");
+// const currentMonth = getCurrentMonth();
+
+// console.log(currentMonth);
+
+const currentMonth = require("./dateFunctions").getCurrentMonth();
+console.log(currentMonth);
